c3c01c361f562ba687e88e682d8f1f84b4024dd9
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-10 18:39:45 -0500

    #

--- Selection and Repetition Program 1.cpp
+++ Selection and Repetition Program 1.cpp
@@ -12,23 +12,22 @@ int main()
 		cin >> num1 >> operand >> num2;
 
 		if (operand == '+')
-			cout << num1 + num2 << endl;
+			cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
 
 		if (operand == '-')
-			cout << num1 - num2 << endl;
+			cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
 
 		if (operand == '*')
-			cout << num1 * num2 << endl;
+			cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
 
 		if (operand == '/')
-			cout << num1 / num2 << endl;
+			cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
 
 		if (operand == '/' && num2 == 0)
 			cout << "Cannot divide by zero." << endl;
-		
-		if (operand != '+' || '-' || '*' || '/')
+
+		if (operand != '+' && operand != '-' && operand != '*' && operand != '/')
 			cout << "Error, invalid operator symbol." << endl;
-		
 	
 
 	return 0;
