86f6d9d0e1f0c0c5076d1854af321a616c1d5e43
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-10 18:04:38 -0500

    #

--- Selection and Repetition Program 1.cpp
+++ Selection and Repetition Program 1.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+double num1, num2;
+string operand;
+
 int main()
 {
+	cout << "Please enter two numbers separated by an operator(+, -, *, /):" << endl;
+		cin >> num1 >> operand >> num2;
 
+		if (operand == +)
+			cout << num1 + num2 << endl;
 
+		if (operand == -)
+			cout << num1 - num2 << endl;
+
+		if (operand == *)
+			cout << num1 * num2 << endl;
+
+		if (operand == /)
+			cout << num1 / num2 << endl;
+	
 
 	return 0;
 }
