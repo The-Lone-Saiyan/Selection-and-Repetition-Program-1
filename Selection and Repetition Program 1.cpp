275e2c3b509e35f6d07625af157dfc44d83a7b78
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-02-17 11:19:31 -0500

    Final

--- Selection and Repetition Program 1.cpp
+++ Selection and Repetition Program 1.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+//Basic Calculator program, user inputs the first number, operator, and second number.
+// Final display is the solution depending on which operator is chosen.
+// Error messages will display if trying to divide by zero or if incorrect operator character is used.
+
 double num1, num2;
 char operand;
 
@@ -11,23 +15,34 @@ int main()
 		cin >> num1 >> operand >> num2;
 
 		if (operand == '+')
+		{
 			cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+		}
 
 		if (operand == '-')
+		{
 			cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+		}
 
 		if (operand == '*')
+		{
 			cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
-
-		if (operand == '/')
-			cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+		}
 
 		if (operand == '/' && num2 == 0)
+		{
 			cout << "Cannot divide by zero." << endl;
+		}
+
+		if (operand == '/')
+		{
+			cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+		}
 
 		if (operand != '+' && operand != '-' && operand != '*' && operand != '/')
+		{
 			cout << "Error, invalid operator symbol." << endl;
-	
+		}
 
 	return 0;
 }
